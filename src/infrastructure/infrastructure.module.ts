8b191b5f4fa9c4dd6dafc3664ce9c30f06a9ae3a
ilesar <ivan.lesar@bornfight.com>
2021-02-12 17:27:36 +0100

    expand infrastructure

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ApiModule } from '@api/api.module';
+import { DatabaseModule } from '@infrastructure/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [ApiModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
