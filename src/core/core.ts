116fc677ca93bc55d5531fff79f4eeda10c37eb1
ilesar <ivan.lesar@bornfight.com>
2021-02-13 20:36:54 +0100

    Add winston logger

--- src/core/core.ts
+++ src/core/core.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { CoreModule } from './core.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { WinstonLoggerService } from '@nest-toolbox/winston-logger';
+import * as winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(CoreModule);
+  const app = await NestFactory.create<NestExpressApplication>(CoreModule, {
+    logger: new WinstonLoggerService({
+      projectName: 'project',
+    }),
+  });
   await app.listen(3000);
 }
 bootstrap();
