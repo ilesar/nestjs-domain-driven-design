2ddfc25dc8ec13fd9997f5fbedcc3be7198cdb13
ilesar <ivan.lesar@bornfight.com>
2021-02-13 20:37:11 +0100

    Remove dead import

--- src/core/core.ts
+++ src/core/core.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { CoreModule } from './core.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { WinstonLoggerService } from '@nest-toolbox/winston-logger';
-import * as winston from 'winston';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(CoreModule, {
