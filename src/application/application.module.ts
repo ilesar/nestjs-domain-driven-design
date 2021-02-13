7eb664c3988969b566db568e2b497791798d3399
ilesar <ivan.lesar@bornfight.com>
2021-02-13 21:01:12 +0100

    Add nest app monitor

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import { ScheduleModule } from 'nest-schedule';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+import { StatusMonitorModule } from 'nest-status-monitor';
 
 @Module({
   imports: [
@@ -21,6 +22,35 @@ import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
           strict: true,
         },
       },
+    }),
+    StatusMonitorModule.setUp({
+      pageTitle: 'Nest.js Monitoring Page',
+      port: 3000,
+      path: '/status',
+      ignoreStartsWith: '/health/alive',
+      spans: [
+        {
+          interval: 1, // Every second
+          retention: 60, // Keep 60 datapoints in memory
+        },
+        {
+          interval: 5, // Every 5 seconds
+          retention: 60,
+        },
+        {
+          interval: 15, // Every 15 seconds
+          retention: 60,
+        },
+      ],
+      chartVisibility: {
+        cpu: true,
+        mem: true,
+        load: true,
+        responseTime: true,
+        rps: true,
+        statusCodes: true,
+      },
+      healthChecks: [],
     }),
     ScheduleModule.register(),
     DomainModule,
