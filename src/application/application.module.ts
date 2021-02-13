fd2b6196d71c71c10d47e5f6b61220904fb42bc8
ilesar <ivan.lesar@bornfight.com>
2021-02-13 20:54:06 +0100

    Add mailer module

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -3,10 +3,25 @@ import { DomainModule } from '@domain/domain.module';
 import { ConfigModule } from 'nestjs-config';
 import * as path from 'path';
 import { ScheduleModule } from 'nest-schedule';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
     ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
+    MailerModule.forRoot({
+      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      defaults: {
+        from: `"nest-modules" <modules@nestjs.com>`,
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
     ScheduleModule.register(),
     DomainModule,
   ],
