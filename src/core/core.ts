2a45a9eaed3fa2a986e88eaec7761b46d131c1dd
ilesar <ivan.lesar@bornfight.com>
2021-02-13 20:55:10 +0100

    Add nest enlighten

--- src/core/core.ts
+++ src/core/core.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { CoreModule } from './core.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { WinstonLoggerService } from '@nest-toolbox/winston-logger';
+import { NestEnlighten } from 'nestjs-enlighten';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(CoreModule, {
@@ -9,6 +10,7 @@ async function bootstrap() {
       projectName: 'project',
     }),
   });
+  app.useGlobalFilters(new NestEnlighten({ theme: 'theme-dark' }));
   await app.listen(3000);
 }
 bootstrap();
