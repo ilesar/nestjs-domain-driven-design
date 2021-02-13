22504023f67ae50aa3a1b79a9bc038168762f6c8
ilesar <ivan.lesar@bornfight.com>
2021-02-13 20:24:12 +0100

    Add scheduler (for cron jobs)

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { DomainModule } from '@domain/domain.module';
 import { ConfigModule } from 'nestjs-config';
 import * as path from 'path';
+import { ScheduleModule } from 'nest-schedule';
 
 @Module({
   imports: [
     ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
+    ScheduleModule.register(),
     DomainModule,
   ],
   providers: [],
