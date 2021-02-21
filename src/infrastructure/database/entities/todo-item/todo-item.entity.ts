546e20ee2495a194efcf1966e1196dd11796c0d8
ilesar <ivan.lesar@bornfight.com>
2021-02-21 22:01:17 +0100

    Docuiment entity for api

--- src/infrastructure/database/entities/todo-item/todo-item.entity.ts
+++ src/infrastructure/database/entities/todo-item/todo-item.entity.ts
@@ -5,23 +5,29 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({
   name: 'todo_items',
 })
 export class TodoItemEntity {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id!: string;
 
+  @ApiProperty()
   @Column()
   title!: string;
 
+  @ApiProperty()
   @Column()
   completed!: boolean;
 
+  @ApiProperty()
   @CreateDateColumn()
   created!: Date;
 
+  @ApiProperty()
   @UpdateDateColumn()
   updated!: Date;
 }
