965885e80807fb1f98f67add08c04d16458bc4c0
ilesar <ivan.lesar@bornfight.com>
2021-07-21 22:45:53 +0200

    Add basic AdminJS

--- src/context/infrastructure/admin/admin.module.ts
+++ src/context/infrastructure/admin/admin.module.ts
@@ -1,45 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { adminCrudMap } from './admin-crud.map';
 import { TodoItemEntity } from '../database/entities/todo-item/todo-item.entity';
 import { TodoItemCrudController } from './resources/todo-item/todo-item.crud-controller';
 import { TodoItemCrudService } from './resources/todo-item/todo-item.crud-service';
-
-// AdminJS.registerAdapter({ Database, Resource });
-// Resource.validate = validate;
+import { AdminModule as AdminJsModule } from '@adminjs/nestjs';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TodoItemEntity])],
+  imports: [
+    TypeOrmModule.forFeature([TodoItemEntity]),
+    AdminJsModule.createAdmin({
+      adminJsOptions: {
+        rootPath: '/admin',
+        resources: [],
+      },
+    }),
+  ],
   controllers: [TodoItemCrudController],
   providers: [TodoItemCrudService],
 })
-// @Module({
-//   imports: [
-//     // GOAL -> BornfightAdminModule.register(config),
-//     TypeOrmModule.forFeature(
-//       adminCrudConfig.map((entityItem) => entityItem.entity),
-//     ),
-//     // AdminJsModule.createAdminAsync({
-//     //   imports: [TypeOrmModule.forFeature([TodoItemEntity])],
-//     //   inject: [getRepositoryToken(TodoItemEntity)],
-//     //   useFactory: (connection: Connection) => ({
-//     //     adminJsOptions: {
-//     //       rootPath: '/admin',
-//     //       databases: [connection],
-//     //       // resources: [{ resource: repository }],
-//     //     },
-//     //     auth: {
-//     //       authenticate: async (email, password) =>
-//     //         Promise.resolve({ email: 'test' }),
-//     //       cookieName: 'test',
-//     //       cookiePassword: 'testPass',
-//     //     },
-//     //   }),
-//     // }),
-//   ],
-//   controllers: adminCrudConfig.map((entityItem) => {
-//     return entityItem.controller;
-//   }),
-//   providers: adminCrudConfig.map((entityItem) => entityItem.service),
-// })
 export class AdminModule {}
