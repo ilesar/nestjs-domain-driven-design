225c149fd4b668303bcaaf566f7c2c7a4d5ee77a
ilesar <ivan.lesar@bornfight.com>
2021-07-26 03:34:57 +0200

    Update repo

--- src/_common/infrastructure/database/repositories/todo-item.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { TodoItemEntity } from '../entities/todo-item/todo-item.entity';
-
-@EntityRepository(TodoItemEntity)
-export class TodoItemRepository extends Repository<TodoItemEntity> {}
