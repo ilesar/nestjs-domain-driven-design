b6481b31302ce5a9c1ed46878644a28e5407dbe2
ilesar <ivan.lesar@bornfight.com>
2021-07-26 03:38:57 +0200

    Update context

--- src/_common/common.bounded-context.ts
+++ src/_common/common.bounded-context.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { InfrastructureLayer } from './infrastructure/infrastructure.layer';
-import { ApplicationLayer } from './application/application.layer';
-import { DomainLayer } from './domain/domain.layer';
 
 @Module({
-  imports: [DomainLayer, ApplicationLayer, InfrastructureLayer],
+  imports: [InfrastructureLayer],
   providers: [],
   exports: [],
 })
