798a2a0007166e8374bc5872b5377fa25b4e516b
ilesar <ivan.lesar@bornfight.com>
2021-07-26 03:34:44 +0200

    Update resolver

--- src/_common/infrastructure/graphql/resources/todo-item/todo-item.resolver.ts
+++ src/_common/infrastructure/graphql/resources/todo-item/todo-item.resolver.ts
@@ -1,15 +1,22 @@
 import { QueryService, InjectQueryService, Filter } from '@nestjs-query/core';
-import { ConnectionType, CRUDResolver } from '@nestjs-query/query-graphql';
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import {
+  ConnectionType,
+  CRUDResolver,
+  UpdateOneInputType,
+} from '@nestjs-query/query-graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { TodoItemDto } from './dtos/todo-item.dto';
 import { TodoItemEntity } from '../../../database/entities/todo-item/todo-item.entity';
 import { TodoItemConnection, TodoItemQuery } from './todo-item.types';
+import { CompleteTodoItemCommand } from '../../../../application/commands/CompleteTodoItemCommand';
+import { CommandBus } from '@nestjs/cqrs';
 
 @Resolver(() => TodoItemDto)
 export class TodoItemResolver extends CRUDResolver(TodoItemDto) {
   constructor(
     @InjectQueryService(TodoItemEntity)
     readonly service: QueryService<TodoItemEntity>,
+    private readonly commandBus: CommandBus,
   ) {
     super(service);
   }
@@ -27,5 +34,13 @@ export class TodoItemResolver extends CRUDResolver(TodoItemDto) {
       ...query,
       ...{ filter },
     });
+  }
+
+  @Mutation(() => TodoItemDto)
+  async completeOneTodoItem(
+    @Args('todoItemId')
+    todoItemId: number,
+  ): Promise<TodoItemDto> {
+    return this.commandBus.execute(new CompleteTodoItemCommand(todoItemId));
   }
 }
