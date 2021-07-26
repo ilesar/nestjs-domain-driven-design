ba44d5ec5a95ea135d84c87847345bf7f6ba09f1
ilesar <ivan.lesar@bornfight.com>
2021-07-26 03:35:19 +0200

    Update model and entity

---
+++ src/_common/domain/models/todo-item.model.ts
@@ -0,0 +1,7 @@
+export class TodoItem {
+  id: number;
+  title: string;
+  completed: boolean;
+  created: Date;
+  updated: Date;
+}
--- src/_common/infrastructure/database/entities/todo-item/todo-item.entity.ts
+++ src/_common/infrastructure/database/entities/todo-item/todo-item.entity.ts
@@ -6,29 +6,23 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({
   name: 'todo_items',
 })
 export class TodoItemEntity extends BaseEntity {
-  @ApiProperty()
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @ApiProperty()
   @Column()
   title!: string;
 
-  @ApiProperty()
   @Column()
   completed!: boolean;
 
-  @ApiProperty()
   @CreateDateColumn()
   created!: Date;
 
-  @ApiProperty()
   @UpdateDateColumn()
   updated!: Date;
 }
